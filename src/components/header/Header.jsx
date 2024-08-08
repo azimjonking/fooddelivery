52f9b4e17b41f1f6e5aa67b7612f921c478699c1
Kamronbek4441 <kamronbekdilshadov24@gmail.com>
2024-08-08 11:58:41 +0500

    header, footer

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,12 @@
 import "./Header.css";
 
 const Header = () => {
-  return <div>
-    <h1>Header</h1>
-  </div>;
+  return (
+    <div className="header">
+      <h1>We're open and available for takeaway & delivery.</h1>
+      <p>Order Now</p>
+    </div>
+  );
 };
 
 export default Header;
